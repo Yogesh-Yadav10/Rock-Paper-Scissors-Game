3dfde7ce3941ac89125c9656ea52c95e3320ca08
Yogesh Yadav <yadavyogeshu20@gmail.com>
2025-08-24 22:59:59 +0530

    Fix: corrected spelling of scissor in HTML and JS

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-  
-    let userScore = 0;
-    let pcScore = 0;
+let userScore = 0;
+let pcScore = 0;
 
-    function game(user, pc) {
-        if (user === pc) return "draw";
-        if (
-            (user === "paper" && pc === "stone") ||
-            (user === "stone" && pc === "sessior") ||
-            (user === "sessior" && pc === "paper")
-        ) {
-            userScore++;
-            return "user";
-        }
-        pcScore++;
-        return "pc";
+function game(user, pc) {
+    if (user === pc) return "draw";
+    if (
+        (user === "paper" && pc === "stone") ||
+        (user === "stone" && pc === "scissors") ||
+        (user === "scissors" && pc === "paper")
+    ) {
+        userScore++;
+        return "user";
     }
+    pcScore++;
+    return "pc";
+}
 
-    function getComputerChoice() {
-        const choices = ["stone", "paper", "sessior"];
-        return choices[Math.floor(Math.random() * choices.length)];
-    }
+function getComputerChoice() {
+    const choices = ["stone", "paper", "scissors"];
+    return choices[Math.floor(Math.random() * choices.length)];
+}
 
-    function play(userChoice) {
-        const pcChoice = getComputerChoice();
-        const result = game(userChoice, pcChoice);
+function play(userChoice) {
+    const pcChoice = getComputerChoice();
+    const result = game(userChoice, pcChoice);
 
-        document.getElementById("result").innerText = `You chose ${userChoice}, PC chose ${pcChoice} 
-         Winner : ${result}`;
-        document.getElementById("user-score").innerText = `User Score: ${userScore}`;
-        document.getElementById("pc-score").innerText = `PC Score: ${pcScore}`;
-    }
-  
+    document.getElementById("result").innerText = `You chose ${userChoice}, PC chose ${pcChoice} 
+     Winner : ${result}`;
+    document.getElementById("user-score").innerText = `User Score: ${userScore}`;
+    document.getElementById("pc-score").innerText = `PC Score: ${pcScore}`;
+}
